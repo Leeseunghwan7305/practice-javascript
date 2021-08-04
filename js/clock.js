03dd4ab4ce42ee8854595dbcd27f01180ffe1538
Leeseunghwan <sun1238799@naver.com>
2021-08-04 23:43:55 +0900

    clock2

--- js/clock.js
+++ js/clock.js
@@ -2,10 +2,23 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
   const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  const chrismas = new Date("December 25,2021 00:00:00");
+  const gap = chrismas - date;
+  if (gap < 0) {
+    alert("merry Chrisams!");
+  } else {
+    const days = Math.floor(gap / (1000 * 60 * 60 * 24)); // 일
+    const hours = String(Math.floor((gap / (1000 * 60 * 60)) % 24)).padStart(
+      2,
+      "0"
+    ); // 시
+    const minutes = String(Math.floor((gap / (1000 * 60)) % 60)).padStart(
+      2,
+      "0"
+    ); // 분
+    const seconds = String(Math.floor((gap / 1000) % 60)).padStart(2, "0"); // 초
+    clock.innerText = `${days}D ${hours}H ${minutes}M ${seconds}S`;
+  }
 }
 getClock();
 setInterval(getClock, 1000);
