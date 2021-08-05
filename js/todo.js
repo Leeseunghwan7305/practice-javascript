06448e11aaf0a03a083221457814146e45ca2082
Leeseunghwan <sun1238799@naver.com>
2021-08-06 00:09:48 +0900

    clock and list

---
+++ js/todo.js
@@ -0,0 +1,18 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoList = document.getElementById("todo-list");
+const toDoInput = toDoForm.querySelector("input");
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
+}
+function handleToDoSumit(event) {
+  event.preventDefault();
+  const newToDo = toDoInput.value;
+  toDoInput.value = "";
+  paintToDo(newToDo);
+}
+toDoForm.addEventListener("submit", handleToDoSumit);
