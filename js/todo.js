58c00657d1edac16a3b45624107d997eb00a9cd7
Leeseunghwan <sun1238799@naver.com>
2021-08-06 16:05:30 +0900

    delete click

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "X";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 function handleToDoSumit(event) {
