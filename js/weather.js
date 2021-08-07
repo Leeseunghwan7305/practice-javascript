eae49a9a83f6868d22b37eb0335f2163ab7fe817
Leeseunghwan <sun1238799@naver.com>
2021-08-07 15:54:30 +0900

    memo-wepsite

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,15 @@
+const API_KEY = "2e3f07a78ec864021e575359b68c521b";
+const weather = document.querySelector("#weather span:first-child");
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
+  const lon = position.coords.longitude;
+  console.log("live in", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}° C`;
+    });
 }
 
 function onGeoError() {
